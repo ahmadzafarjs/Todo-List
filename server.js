4bcea7bdacc72beecc1a2da8f0b64b7aa08b70f1
Ahmad <imahmad336@gmail.com>
2023-11-27 14:44:50 +0500

    home page css completed

--- server.js
+++ server.js
@@ -1,1 +1,18 @@
-console.log("Hello todo list");
+// Require Packeges
+const express = require("express");
+const ejs = require("ejs");
+const bodyParser = require("body-parser");
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Middleweres
+app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+  res.render("pages/home");
+});
+
+app.listen(port, () => console.log("Server starts on localhost:3000"));
